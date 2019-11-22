28be254b8fd168d971163b490680151698a5f560
Lee Pedersen <leepedersenr@gmail.com>
2019-11-22 11:34:03 -0800

    create find-by-symptom function

--- src/apicall.js
+++ src/apicall.js
@@ -1,8 +1,12 @@
 export class Doctor {
-  async getDoctorBySymptom(symptom) {
-    let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=Smith&location=45.505%2C-122.675%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4673d02a855d648150ab936ef94c04a7`);
+  async getDoctorByName(name) {
+    let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=45.505%2C-122.675%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4673d02a855d648150ab936ef94c04a7`);
     let jsonifiedResponse = await response.json();
-
+    return jsonifiedResponse;
+  }
+  async getDoctorBySymptom(symptom) {
+    let response = await fetch (`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=45.505%2C-122.675%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=4673d02a855d648150ab936ef94c04a7`);
+    let jsonifiedResponse = await response.json();
     return jsonifiedResponse;
   }
 }
--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
 
   (async () => {
     let newDoc = new Doctor();
-    const response = await newDoc.getDoctorBySymptom("rash");
+    const response = await newDoc.getDoctorByName("Cat");
     getElements(response);
   })();
 
@@ -18,7 +18,8 @@ $(document).ready(function() {
     let resultArray = [];
     for (let i = 0; i < response.data.length; i++) {
       resultArray.push(response.data[i].profile.first_name);
-      $(".results").text(resultArray);
+      resultArray.push(response.data[i].profile.last_name);
+      $(".results").append("<br>" + response.data[i].profile.first_name + " " + response.data[i].profile.last_name + "<br>");
     }
 
   }
