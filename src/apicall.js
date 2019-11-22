535ffc11c3d70ea26a2a95ba73b622f1a7414adc
Lee Pedersen <leepedersenr@gmail.com>
2019-11-22 10:21:56 -0800

    separate UI and business logic

--- src/apicall.js
+++ src/apicall.js
@@ -1,28 +1,7 @@
-$(document).ready(function() {
-  $('#weatherLocation').click(function() {
-    let city = $('#location').val();
-    $('#location').val("");
-
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=[API-KEY-GOES-HERE]`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(Error(request.statusText));
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    });
-  });
-});
+export class Doctor {
+  async getDoctorBySymptom(symptom) {
+    let response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`);
+    let jsonifiedResponse = await response.json();
+    return jsonifiedResponse;
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,30 +2,19 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Doctor } from './../src/apicall.js';
 
 $(document).ready(function() {
-  console.log("at least this is working");
 
-  let promise = new Promise(function(resolve, reject) {
-    let request = new XMLHttpRequest();
-    let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=37.773,-122.413,100&skip=2&limit=10&user_key=${process.env.API_KEY}`;
-    request.onload = function() {
-      if (this.status === 200) {
-        resolve(request.response);
-      } else {
-        reject(Error(request.statusText));
-      }
-    }
-    request.open("GET", url, true);
-    request.send();
-  });
 
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    console.log(body);
-    // $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-    // $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-  }, function(error) {
-    $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-  });
+  (async () => {
+    let newDoc = new Doctor();
+    const response = await newDoc.getDoctorBySymptom("cough");
+    getElements(response);
+  })();
+
+  function getElements(response) {
+    console.log(response);
+  }
+
 });
